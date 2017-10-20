12d88ddd471a081b7b3df959888087fd981308ff
LMokrane <mokrane.lateb@sqy.fr>
2017-10-20 20:42:00 +0200

    compute regularized cost fun

--- tests/ex3.js
+++ tests/ex3.js
@@ -6,10 +6,19 @@ const Client = require('Client');
 const mnist = require('mnist');
 
 test('ex3 - Multi-class Classification and Neural Networks', t => {
-  t.plan(2);
+  t.plan(3);
 
   try {
     let client = new Client();
+    let X_t = [[1.0, 0.1, 0.6, 1.1], [1.0, 0.2, 0.7, 1.2], [1.0, 0.3, 0.8, 1.3], [1.0, 0.4, 0.9, 1.4], [1.0, 0.5, 1.0, 1.5]];
+    let y_t = [[1], [0], [1], [0], [1]];
+    let theta_t = [[-2], [-1], [1], [2]];
+    let lambda_t = 3;
+    client.m = X_t.length;
+    client.n = X_t[0].length;
+    let J = client.r_logistic_reg_costFunction(X_t, y_t, theta_t, lambda_t);
+    t.equal(J, 2.534819396109744, 'Expected cost: 2.534819');
+
     let set = mnist.set(5000, 1000);
     let trainingSet = set.training;
     const fulfill = o => {
