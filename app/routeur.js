e617e2644401cb1e438f6243344d03778a52f8c0
Mokrane Lateb <mokrane.lateb@agglo-sqy.fr>
2017-11-15 16:24:39 +0100

    routeur

---
+++ app/routeur.js
@@ -0,0 +1,22 @@
+let routeur = require('express').Router();
+const Learning = require('Learning');
+
+routeur.get('/', (req, res) => {
+  req.log.info(`GET ${req.baseUrl}`);
+  res.render('index', {titre: process.env.APP});
+});
+
+routeur.post('/', (req, res) => {
+  let X = req.body;
+  let Theta1 = [];
+  let Theta2 = [];
+  let machine = new Learning(X);
+  try {
+    let p = predict_nn(X, Theta1, Theta2);
+    res.send(p);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+module.exports = routeur;
