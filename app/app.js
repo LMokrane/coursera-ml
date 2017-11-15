e94c48dd8b160f7e571e47b2610ff68c4127cbe0
Mokrane Lateb <mokrane.lateb@agglo-sqy.fr>
2017-11-15 15:53:22 +0100

    app

---
+++ app/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(helmet);
+app.use(bodyParser.json());
+
+app.set('views', `${__dirname}/vues`);
+app.set('view engine', 'pug');
+
+module.exports = app;
